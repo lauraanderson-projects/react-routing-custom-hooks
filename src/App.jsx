987f014fb60c6cf04fb84fc7e68fc49475095a19
sphirebluela <laura.sphireblue@gmail.com>
2025-05-12 16:24:55 -0700

    common header creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { Routes, Route, useNavigate, Link } from "react-router-dom";
+import { Routes, Route, useNavigate, Link, useRoutes } from "react-router-dom";
 import "./App.css";
 import RecipeList from "./pages/recipes";
 import CommentsList from "./pages/comments";
 import RecipeDetails from "./pages/recipe-details";
 import NotFoundPage from "./pages/not-found";
+import Layout from "./components/layout";
+
+function CustomRoutes() {
+  const element = useRoutes([
+    {
+      path: "/home",
+      element: <Layout />,
+      children: [
+        { path: "recipes", element: <RecipeList /> },
+        { path: "comments", element: <CommentsList /> },
+        { path: "recipes/:id", element: <RecipeDetails /> },
+      ],
+    },
+    { path: "*", element: <NotFoundPage /> },
+  ]);
+  return element;
+}
 
 function App() {
   const navigate = useNavigate();
@@ -13,26 +30,29 @@ function App() {
       <h1>React routing, Custom hooks and more</h1>
 
       <div>
-        <Link to={"/recipes"}>Alt Recipes</Link>
+        <Link to={"/home/recipes"}>Alt Recipes</Link>
       </div>
       <button
-        onClick={() => navigate("/recipes")}
+        onClick={() => navigate("/home/recipes")}
         style={{ backgroundColor: "black", color: "white" }}
       >
         Recipes
       </button>
       <button
-        onClick={() => navigate("/comments")}
+        onClick={() => navigate("/home/comments")}
         style={{ backgroundColor: "black", color: "white" }}
       >
         Comments
       </button>
-      <Routes>
-        <Route path="/recipes" element={<RecipeList />} />
-        <Route path="/comments" element={<CommentsList />} />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      {/*<Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="recipes" element={<RecipeList />} />
+          <Route path="comments" element={<CommentsList />} />
+          <Route path="recipes/:id" element={<RecipeDetails />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      </Routes>*/}
+      <CustomRoutes />
     </div>
   );
 }
