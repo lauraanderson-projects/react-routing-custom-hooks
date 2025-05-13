f3cd640cf93b15c26d929752e85aaf5a4f736a84
sphirebluela <laura.sphireblue@gmail.com>
2025-05-12 22:06:11 -0700

    setup fetch hook for recipes

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -1,12 +1,26 @@
 import { useLocation } from "react-router-dom";
+import useFetch from "../../hooks/use-fetch";
 
 function RecipeList() {
   const location = useLocation();
-  console.log(location);
+  const { data, loading, error } = useFetch("https://dummyjson.com/recipes");
+
+  if (loading) return <p>Loading...</p>;
   return (
     <div>
       <h1>Recipe List</h1>
       <p>Here you can find a list of delicious recipes.</p>
+      <ul>
+        {data?.recipes?.length > 0
+          ? data?.recipes.map((recipeItem) => (
+              <div>
+                <img src={recipeItem?.image} alt={recipeItem?.name} />
+                <br />
+                <label>{recipeItem?.name}</label>
+              </div>
+            ))
+          : null}
+      </ul>
     </div>
   );
 }
