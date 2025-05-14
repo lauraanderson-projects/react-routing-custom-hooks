98bdb9ddceda1b5aaf0ba4775b54ed87d4432b29
sphirebluela <laura.sphireblue@gmail.com>
2025-05-13 20:32:23 -0700

    completed fetch for comments and hook for window size

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,7 +1,22 @@
+import useFetch from "../../hooks/use-fetch";
+
 function CommentsList() {
+  const { data, loading, error } = useFetch("https://dummyjson.com/comments");
+  if (loading) return <h1>Fetching Comments, please wait.</h1>;
+
   return (
     <div>
       <h1>Comments List</h1>
+      <ul>
+        {data?.comments?.length > 0
+          ? data?.comments.map((commentItem) => (
+              <div>
+                <label>{commentItem?.body}</label>
+                <p>{commentItem?.likes}</p>
+              </div>
+            ))
+          : null}
+      </ul>
     </div>
   );
 }
--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -1,14 +1,27 @@
 import { useLocation } from "react-router-dom";
 import useFetch from "../../hooks/use-fetch";
+import useWindowResize from "../../hooks/use-window-resize";
+
+//uselocalstorage
+//useCounter
+//useSessinStorage
+//useId
+//useOutsideClick
 
 function RecipeList() {
   const location = useLocation();
   const { data, loading, error } = useFetch("https://dummyjson.com/recipes");
+  const windowSize = useWindowResize();
 
   if (loading) return <p>Loading...</p>;
   return (
     <div>
-      <h1>Recipe List</h1>
+      <h1 style={{ color: windowSize.width < 900 ? "red" : "black" }}>
+        Recipe List
+      </h1>
+      <p>
+        Window Size: {windowSize.width} x {windowSize.height}
+      </p>
       <p>Here you can find a list of delicious recipes.</p>
       <ul>
         {data?.recipes?.length > 0
