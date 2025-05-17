c51f3e818e925bd91600a29978507a1e33e763d2
sphirebluela <laura.sphireblue@gmail.com>
2025-05-16 22:01:19 -0700

    react hook forms

--- src/pages/react-hook-form-example/index.jsx
+++ src/pages/react-hook-form-example/index.jsx
@@ -1,19 +1,50 @@
 import { useForm } from "react-hook-form";
 
 function ReactHookForm() {
-  const {} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  function onSubmitForm(formData) {
+    console.log("Form Data", formData);
+    // Simulate a form submission
+    //you can replace this with an actual API call
+    reset();
+  }
 
   return (
     <div>
       <h1>React Hook Form</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmitForm)}>
         <div>
           <label>Email</label>
-          <input type="email" name="email" />
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            name="email"
+          />
+          {errors.email && errors.email.type === "required" ? (
+            <p style={{ color: "red" }}>Email is required</p>
+          ) : null}
         </div>
         <div>
           <label>Password</label>
-          <input type="password" name="password" />
+          <input
+            {...register("password", { required: true, minLength: 7 })}
+            type="password"
+            name="password"
+          />
+          {errors.password && errors.password.type === "required" ? (
+            <p style={{ color: "red" }}>Password is required</p>
+          ) : null}
+          {errors.password && errors.password.type === "minLength" ? (
+            <p style={{ color: "red" }}>
+              Password must be at least 5 characters
+            </p>
+          ) : null}
         </div>
         <button type="submit">Submit</button>
       </form>
